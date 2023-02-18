cfb84425dfa38fe55e5a8f88ec6443f4d374a77d
Zizipho <mabangazizipho@gmail.com>
2023-02-17 16:46:19 -0800

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - prints the lowercase alphabet in reverse
+ * Return: Always 0
+ */
+int main(void)
+{
+	char c = 'z';
+
+	while (c >= 'a')
+	{
+		putchar(c);
+		c--;
+	}
+
+	putchar('\n');
+	return (0);
+}
