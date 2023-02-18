c03995897357f5b4d7c4938ce094e7dfce0db0cf
Zizipho <mabangazizipho@gmail.com>
2023-02-17 16:54:18 -0800

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib>
+/**
+ * main -  prints all the numbers of base 16 in lowercase
+ * Return: Always 0
+ */
+int main(void)
+{
+	char c = 'z';
+
+	while (c >= 'a')
+	{
+		putchar(c);
+		c--;
+	}
+
+	putchar('\n');
+	return (0);
+}
