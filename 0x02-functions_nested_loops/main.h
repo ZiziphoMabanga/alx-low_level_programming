4b3ae3c80a24f9886f0226318e4876a0a7cf64a9
Zizipho <mabangazizipho@gmail.com>
2023-02-21 18:57:07 -0800

    Functions, nested loops

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,8 @@
 #ifndef main_h
 #define main_h
 
-void print_alpihabet(void);
+int _putchar(char);
+void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
